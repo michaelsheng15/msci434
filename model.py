ed5718f856a262fc18f4f67ca4b3939609957429
Michael Sheng <m3sheng@uwaterloo.ca>
2024-07-29 15:34:09 -0400

    adding timer function

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import gurobipy as gp
 from gurobipy import GRB
+import time
+
+start = time.time()
 
 suppliers = ['S1', 'S2']
 warehouses = ['W1', 'W2']
@@ -46,58 +49,48 @@ model.setObjective(
 
 # CONSTRAINTS
 # Total amount produced must be greater than or equal to total retail demand
-model.addConstr(
-    gp.quicksum(s_f[f] for f in suppliers) >= gp.quicksum(b_r[r] for r in retailers), "SupplyDemandBalance"
-)
+model.addConstr(gp.quicksum(s_f[f] for f in suppliers) >= gp.quicksum(b_r[r] for r in retailers))
 
 # Amount shipped from supplier to warehouse is the total amount produced
 for f in suppliers:
-    model.addConstr(
-        gp.quicksum(x_sw[f, w] for w in warehouses) == s_f[f], f"FlowConservation_{f}"
-    )
+    model.addConstr(gp.quicksum(x_sw[f, w] for w in warehouses) == s_f[f])
 
 # Amount produced of each supplier cannot exceed maximum capacity of supplier
 for f in suppliers:
-    model.addConstr(
-        s_f[f] <= m_s[f], f"SupplyCapacity_{f}"
-    )
+    model.addConstr(s_f[f] <= m_s[f])
 
 # Amount shipped is equal to the demand of each retailer
 for r in retailers:
-    model.addConstr(
-        gp.quicksum(x_wr[w, r] for w in warehouses) == b_r[r], f"DemandFulfillment_{r}"
-    )
+    model.addConstr(gp.quicksum(x_wr[w, r] for w in warehouses) == b_r[r])
 
 # Amount shipped into each warehouse is equal to the amount shipped out of each node
 for w in warehouses:
-    model.addConstr(
-        gp.quicksum(x_sw[s, w] for s in suppliers) == gp.quicksum(x_wr[w, r] for r in retailers), f"WarehouseBalance_{w}"
-    )
+    model.addConstr(gp.quicksum(x_sw[s, w] for s in suppliers) == gp.quicksum(x_wr[w, r] for r in retailers))
 
 # Amount shipped into each warehouse is less than or equal to maximum capacity of the warehouse
 for w in warehouses:
-    model.addConstr(
-        gp.quicksum(x_wr[w, r] for r in retailers) <= m_td[w], f"TransshipmentCapacity_{w}"
-    )
+    model.addConstr(gp.quicksum(x_wr[w, r] for r in retailers) <= m_td[w])
 
+
+start = time.time()
 model.optimize()
+end = time.time()
+length = end - start
 
-if model.status == GRB.OPTIMAL:
-    print("\n\nRESULTS:")
-    print(f"\nOptimal Total Profit: {model.objVal}")
+#RESULTS
+print("\n\nRESULTS:")
+print("Model Solution Duration: ", length, "seconds!")
 
-    print("\nOptimal shipment quantities:")
-    for f in suppliers:
-        for w in warehouses:
-            print(f"  From {f} to {w}: {x_sw[f, w].x} units")
+print(f"\nOptimal Total Profit: {model.objVal}")
+
+print("\nOptimal shipment quantities:")
+for f in suppliers:
     for w in warehouses:
-        for r in retailers:
-            print(f"  From {w} to {r}: {x_wr[w, r].x} units")
+        print(f"  From {f} to {w}: {x_sw[f, w].x} units")
+for w in warehouses:
+    for r in retailers:
+        print(f"  From {w} to {r}: {x_wr[w, r].x} units")
             
-    print("\nOptimal production quantities:")
-    for s in suppliers:
-        print(f"  Suppliers {s} should produce: {s_f[s].x} units")
-
-
-else:
-    print("No optimal solution found.")
+print("\nOptimal production quantities:")
+for s in suppliers:
+    print(f"  Suppliers {s} should produce: {s_f[s].x} units")
